42c7f22989186229b7f3f5e94138221754dcb997
Leigh West <leighwest@hotmail.com>
2022-08-13 09:35:06 +0000

    Added unit test for App component

---
+++ app/client/src/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
+
+test('Initial conditions', () => {
+  render(<App />);
+
+  const confirmButton = screen.getByRole('button', {
+    name: /generate invoice/i,
+  });
+  expect(confirmButton).not.toBeDisabled();
+});
--- app/client/src/Components/UI/Card.test.js
+++ app/client/src/Components/UI/Card.test.js
@@ -2,9 +2,11 @@ import { render, screen } from '@testing-library/react';
 import Card from './Card';
 
 test('renders "Test card"', () => {
+  render(
+    <Card>
+      <p>Test card</p>
+    </Card>,
+  );
 
-  const component = <Card><p>Test card</p></Card>
-  const cardComponent = render(component);
-
-  expect(cardComponent.getByText('Test card')).not.toBeNull();
+  expect(screen.getByText('Test card')).not.toBeNull();
 });
