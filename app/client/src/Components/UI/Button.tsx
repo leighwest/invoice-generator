d11316bc17d4fad4126c1cd01be1aa057e19e834
Leigh West <leighwest@hotmail.com>
2022-08-22 10:44:06 +0000

    Converted button to TS component, added styled component to button

---
+++ app/client/src/Components/UI/Button.tsx
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+const StyledButton = styled.button`
+  border-radius: 4px;
+  padding: 0.72rem 1.7rem;
+  border: 1px solid var(--color-purple);
+  background-color: #fff;
+  color: var(--color-purple);
+  font-weight: bold;
+  font-size: 1rem;
+  width: 193px;
+  margin: 16px 0 0 auto;
+  text-transform: uppercase;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
+
+const Button: React.FC<{ buttonText: string[]; onClick: () => void }> = (
+  props,
+) => {
+  return (
+    <>
+      <StyledButton
+        type="button"
+        onClick={props.onClick}>
+        {props.buttonText}
+      </StyledButton>
+    </>
+  );
+};
+
+export default Button;
