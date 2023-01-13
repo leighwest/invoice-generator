d2ce8d103edb6ffd661388eaed9d58afc26a685a
Leigh West <leighwest@hotmail.com>
2023-01-13 08:08:01 +0000

    Fixed mutation of object in unit test

--- app/server/src/services/userValidationService.test.ts
+++ app/server/src/services/userValidationService.test.ts
@@ -1,20 +1,30 @@
 import { validateUser } from './userValidationService';
 import { User } from '../models/user';
 
-const MOCKUSER: User = {
-  id: 'u1',
-  firstName: 'Peter',
-  lastName: 'Griffin',
-  email: 'p.griffin@example.com',
-  password: 'password123',
-};
-
 describe('New user validator tests', () => {
+  const user: User = {
+    id: 'u1',
+    firstName: 'Peter',
+    lastName: 'Griffin',
+    email: 'p.griffin@example.com',
+    password: 'password123',
+  };
+
   test('Returns a "first name invalid" error message if first name is empty', async () => {
-    MOCKUSER.firstName = '';
-    const validationResult = validateUser(MOCKUSER);
+    const userBlankFirstName: User = { ...user, firstName: '' };
+
+    const validationResult = validateUser(userBlankFirstName);
 
     expect(validationResult.length).toEqual(1);
     expect(validationResult[0].message).toBe('First name cannot be empty');
+  });
+
+  test('Returns a "last name invalid" error message if first name is empty', async () => {
+    const userBlankLastName: User = { ...user, lastName: '' };
+
+    const validationResult = validateUser(userBlankLastName);
+
+    expect(validationResult.length).toEqual(1);
+    expect(validationResult[0].message).toBe('Last name cannot be empty');
   });
 });
