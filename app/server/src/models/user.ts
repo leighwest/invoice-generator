cc1f78303aa55946c70d4e367fb478936f503fbd
Leigh West <leighwest@hotmail.com>
2023-02-19 05:12:05 +0000

    Updated User schema to index email and ensure it is unique

--- app/server/src/models/user.ts
+++ app/server/src/models/user.ts
@@ -1,3 +1,7 @@
+import { v4 as uuidV4 } from 'uuid';
+import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator'
+
 export type User = {
   id: string;
   firstName: string;
@@ -5,3 +9,18 @@ export type User = {
   email: string;
   password: string;
 };
+
+const userSchema = new mongoose.Schema({
+  id: {type: uuidV4, required: true},
+  firstName: {type: String, required: true},
+  lastName: {type: String, required: true},
+  email: {type: String, required: true, unique: true},  // unique just creates an index (faster querying), it doesn't 
+                                                        // validate that the email is unique in the DB
+  password: {type: String, required: true},
+});
+
+userSchema.plugin(uniqueValidator);   // this uses a mongoose package to validate that email is unique
+
+module.exports = mongoose.model('User', userSchema);
+
+
