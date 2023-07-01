2e646fc9fb7230adfefe886e40bd7baaf80327a3
Leigh West <leighwest@hotmail.com>
2023-07-01 23:26:44 +0000

    Initial css for template, refactored createInvoiceService to include styling

--- app/server/src/services/createInvoiceService.ts
+++ app/server/src/services/createInvoiceService.ts
@@ -1,23 +1,33 @@
 import puppeteer from 'puppeteer';
 
+// no longer using this:
 import { templateService } from './templateService';
-// const template = require("../template/invoice.jsx");
-// const ReactDOMServer = require("react-dom/server");
-// const React = require("react");
+import { ServerStyleSheet } from 'styled-components';
+
+import { invoiceTemplate } from 'template/invoice';
+
+import React from 'react';
+import ReactDOMServer from 'react-dom/server';
 
 export const invoiceGenerator = async (invoiceData: any) => {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ['--no-sandbox'],
-  });
+  const sheet = new ServerStyleSheet();
+  const invoice = React.createElement(invoiceTemplate, invoiceData);
+  const html = ReactDOMServer.renderToStaticMarkup(
+    sheet.collectStyles(invoice),
+  );
+  const styleTags = sheet.getStyleTags();
+
+  const browser = await puppeteer.launch();
   const page = await browser.newPage();
-  const template = templateService(invoiceData);
 
-  await page.setContent(template);
+  // Set the page content to the generated HTML and add the extracted CSS styles
+  await page.setContent(html);
+  await page.addStyleTag({ content: styleTags });
 
-  const pdfToSend = await page.pdf({ path: 'hn.pdf', format: 'a4' });
+  // Generate a PDF file from the page
+  const pdf = await page.pdf({ path: 'invoice.pdf', format: 'A4' });
 
   await browser.close();
 
-  return pdfToSend;
+  return pdf;
 };
--- app/server/src/template/invoice.tsx
+++ app/server/src/template/invoice.tsx
@@ -1,4 +1,107 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  padding: 40px;
+`;
+
+const SubHeading = styled.h3`
+  color: #7182ff;
+  font-weight: normal;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const CompanyDetails = styled.div`
+  text-align: right;
+`;
+
+const InvoiceDetailsWrapper = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 50px;
+  padding-bottom: 60px;
+  border-bottom: solid 3px #7182ff;
+`;
+
+const ClientDetails = styled.div`
+  width: 50%;
+`;
+
+const DateSection = styled.div`
+  width: 20%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const InvoiceNumberSection = styled.div`
+  width: 21%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const AmountDueSection = styled.div`
+  width: 20%;
+  text-align: right;
+  display: flex;
+  flex-direction: column;
+`;
+
+const LineItemWrapper = styled.div`
+  margin-top: 20px;
+`;
+
+const LineItemHeader = styled.div`
+  display: flex;
+  color: #7182ff;
+  text-transform: uppercase;
+`;
+
+const LineItem = styled.div`
+  display: flex;
+  margin-top: 10px;
+  padding-bottom: 10px;
+  border-bottom: solid 1px #ccc;
+`;
+
+const LineItemDescription = styled.div`
+  flex: 0 0 44%;
+`;
+
+const LineItemRate = styled.div`
+  flex: 1.5;
+  text-align: right;
+`;
+
+const LineItemQty = styled.div`
+  flex: 1;
+  text-align: right;
+`;
+
+const LineItemAmount = styled.div`
+  flex: 1;
+  text-align: right;
+`;
+
+const TotalsWrapper = styled.div`
+  margin-left: 45%;
+`;
+
+const TotalsSection = styled.div`
+  border-bottom: solid 1px #ccc;
+`;
+
+const TotalsRow = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+const FreeTextSection = styled.div`
+  margin-top: 50px;
+`;
 
 export const invoiceTemplate = (invoiceData: {
   address: {
@@ -12,38 +115,100 @@ export const invoiceTemplate = (invoiceData: {
   dateDue: string;
   service: { description: string; cost: number }[];
 }) => {
-  const titleStyle = {
-    fontSize: '10rem',
-    lineHeight: '1.6',
-    color: 'red',
-  };
-
   const { recipient, streetAddress, suburb, state, postcode } =
     invoiceData.address;
 
   return (
-    <div>
-      <link
-        rel="stylesheet"
-        href="./invoice.css"
-      />
+    <Wrapper>
+      <Header>
+        <img
+          src="/logo.png"
+          alt="logo"
+          width={200}
+        />
+        <CompanyDetails>
+          YOUR COMPANY <br />
+          16 Somers St <br />
+          Burwood, VIC <br />
+          3125 <br />
+          Australia <br />
+          ABN 50 110 219 460
+        </CompanyDetails>
+      </Header>
+      <InvoiceDetailsWrapper>
+        <ClientDetails>
+          <SubHeading>Billed To</SubHeading>
+          {recipient} <br />
+          {streetAddress} <br />
+          {suburb}, {state} <br />
+          {postcode}
+        </ClientDetails>
+        <DateSection>
+          <SubHeading>Date Issued</SubHeading>
+          {invoiceData.dateIssued}
+          <SubHeading>Date Due</SubHeading>
+          {invoiceData.dateDue}
+        </DateSection>
+        <InvoiceNumberSection>
+          <SubHeading>Invoice Number</SubHeading>
+          INV-10012
+        </InvoiceNumberSection>
+        <AmountDueSection>
+          <SubHeading>Amount Due</SubHeading>
+          $1699.48
+        </AmountDueSection>
+      </InvoiceDetailsWrapper>
+      <LineItemWrapper>
+        <LineItemHeader>
+          <LineItemDescription>Description</LineItemDescription>
+          <LineItemRate>Rate</LineItemRate>
+          <LineItemQty>Qty</LineItemQty>
+          <LineItemAmount>Amount</LineItemAmount>
+        </LineItemHeader>
+        {invoiceData.service.map((service, index) => {
+          return (
+            <LineItem key={index}>
+              <LineItemDescription>{service.description}</LineItemDescription>
 
-      <link
-        rel="stylesheet"
-        type="text/css"
-        href="css/publicInvoice.css"
-      />
-
-      <h1 className="red">{recipient}</h1>
-      <h1 style={titleStyle}>{streetAddress}</h1>
-      <h1>{streetAddress}</h1>
-      <h1>{suburb}</h1>
-      <h1>{state}</h1>
-      <h1>{postcode}</h1>
-      <h1>{invoiceData.dateIssued}</h1>
-      <h1>{invoiceData.dateDue}</h1>
-      <h1>{invoiceData.service[0].description}</h1>
-      <h1>{invoiceData.service[0].cost}</h1>
-    </div>
+              <LineItemAmount>{service.cost}</LineItemAmount>
+            </LineItem>
+          );
+        })}
+      </LineItemWrapper>
+      <TotalsWrapper>
+        <TotalsSection>
+          <TotalsRow>
+            <p>Subtotal</p>
+            <p> $120.00</p>
+          </TotalsRow>
+          <TotalsRow>
+            <p>GST</p>
+            <p style={{ marginRight: '110px' }}>10%</p>
+            <p>$12.00</p>
+          </TotalsRow>
+        </TotalsSection>
+        <TotalsSection>
+          <TotalsRow>
+            <p>Total</p>
+            <p> $132.00</p>
+          </TotalsRow>
+        </TotalsSection>
+        <TotalsSection>
+          <TotalsRow>
+            <p>Amount Due</p>
+            <p> $132.00</p>
+          </TotalsRow>
+        </TotalsSection>
+      </TotalsWrapper>
+      <FreeTextSection>
+        <SubHeading>Notes</SubHeading>
+        <p>Kindly include your payment reference if paying electronically.</p>
+        <p>Thank you for your business.</p>
+      </FreeTextSection>
+      <FreeTextSection>
+        <SubHeading>Terms</SubHeading>
+        <p>Payment due within 7 days</p>
+      </FreeTextSection>
+    </Wrapper>
   );
 };
